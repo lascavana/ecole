a198d535b26173df04fc748f8b5d22fd5d254373
lascavana <lascavuzzo@gmail.com>
2020-06-30 19:03:31 +0200

    Added pseudocost observation

---
+++ libecole/include/ecole/observation/pseudocosts.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+
+#include <nonstd/optional.hpp>
+#include <xtensor/xtensor.hpp>
+
+#include "ecole/observation/abstract.hpp"
+
+namespace ecole {
+namespace observation {
+
+class Pseudocosts : public ObservationFunction<nonstd::optional<xt::xtensor<double, 1>>> {
+public:
+	nonstd::optional<xt::xtensor<double, 1>> obtain_observation(scip::Model& model) override;
+};
+
+}  // namespace observation
+}  // namespace ecole
